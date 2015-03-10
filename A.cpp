4ca1a13920c043be3f90dd4976e8c35afd09675a
litingyang <F74032023@mail.ncku.edu.tw>
2015-03-10 20:45:25 +0800

    01

---
+++ A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+int n;
+cin>>n;
+	while (n!=1){
+		if (n % 2 == 1){
+				n = 3*n + 1;
+				cout << n << endl;
+			}
+
+		else {n = n / 2;
+				cout << n << endl;
+			}
+
+		}
+	}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+input a number first
+then it will print out the answer
+g++ -o A A.cpp
+./A
